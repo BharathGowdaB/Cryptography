52688398a0cb33d882a1aa6a578f45ab494228cd
Bharath Gowda B <bharathvishnubharath@gmail.com>
2021-11-22 08:37:34 +0530

    v1.1

--- client.py
+++ client.py
@@ -1,59 +1,123 @@
 from socket import *
 import sys
+import json
+import threading
+import re
+
 serverName = 'servername'
 serverPort = 8001
 
-clientSocket = socket()
-
+clientSocket = socket(AF_INET,SOCK_STREAM)
 clientSocket.connect(('localhost',int(serverPort)))
 
+uniId = ''
+isactive = True
+user = {}
+
 #uniId = na-na-na-na-na          n-> no of characters  a->shifting value
-def encryptDecryptData(uniId,msg,type) :
-    length = len(uniId)
-    n = []
-    a = []
-    sum = 0
-    for i in range(0,length,2) :
-        n.append(int(uniId[i]))
+def encryptDecryptData(uniId,msg,multiplier) :
+    senderPat = ''
+    for i in range(0,len(uniId),2) :
+        x = int(uniId[i])
+        for j in range(x) :
+            senderPat += uniId[i+1]
 
-    for i in range(1,length,2) :
-        a.append(int(uniId[i]) * type) 
-    sum = 0
-    
-    for i in range(len(n)) :
-        sum += n[i]
-        
-    print(n , a)
-    m = len(msg)//sum
     text = ''
-    count = 0
-    x = 0
-    y = 0
-    while True :
-        for j in range(n[x]) :
-            if(count >= len(msg)) :
-                return text
-            text += chr(ord(msg[count]) + a[j])
-            count += 1
-        x = (x + 1) % len(n)
+    s = 0
+    for i in range(len(msg)) :
+        text += chr(ord(msg[i]) - int(senderPat[s]) * multiplier)
+        s = (s + 1) % len(senderPat)
+
+    return text 
+
+def sendingThread() :
+    global clientSocket
+    print('enter msg to send')
+    msg = input()
+    data = re.split('\s*[:,]\s*',msg,3)
+    print(data)
+    
+    if len(data) > 3 and  re.search('.*TO',data[0].upper()) != None and re.search('M.*S.*G.*',data[2].upper()) != None :
+        try : 
+            message = {
+                'to' : int(data[1]),
+                'msg' : encryptDecryptData(user['id'],data[3],1)
+            }
+            
+            data = json.dumps(message)
+            clientSocket.send(data.encode())
+            print('msg sent')
+        except :
+            print('Invalid Number :')
+    else :
+        print("Please re-enter the message with proper format,\n\n To : 'number',Message : 'your message' \n")
+
+def historyThread() :
+    print('History to be created')
+
+def receivingThread() :
+    global clientSocket,isactive,user
+    while isactive :
+        try : 
+            data = clientSocket.recv(2048)
+            msg = data.decode()
+            data = json.loads(data)
+            if not data['error'] :
+                print(encryptDecryptData(user['id'],data['msg'],-1))
+            else :
+                print(data['type'])
+        except :
+            print('Error in receiving Thread')
+
+    print('Session Ended :')
+
+def helpThread() :
+    print('Available Command with Syntax')
+    print("send :     //send message ")
+    print("history :  //get your message history")
+    print("help :     // to get additional help")   
+    print("exit :     //to end the session")
+
+def registerUserThread(client) :
+    num = input('Enter your number : ')
+    client.send(num.encode())
+    response = client.recv(256)
+    r = response.decode()
+    res = json.loads(r)
+    return res
 
 
-text = encryptDecryptData('12342515','How are you?' ,1)
-text = encryptDecryptData('12342515',text,-1)
-print(text)
-while 1 :
+user = registerUserThread(clientSocket) 
+print(user)
+if not user['error'] :
+    print("use 'help' to get help")
+    id = user['id']
+    recevier = threading.Thread(target=receivingThread, args=())
+    recevier.start()
+    while 1 :
+        try :
+            com = input()
+            com = com.upper()
+            if com == 'HELP' :
+                helpThread()
+            elif com == 'SEND' :
+                sendingThread()
+            elif com == 'HISTORY' :
+                historyThread()
+            elif com == 'EXIT' :
+                isactive = False
+                clientSocket.send('exit'.encode())
+                break
+            else :
+                print("Command doesn't exit, use 'help' command")
+        
+        except :
+            print('error in while')
     
-    msg = input('Input lowercase sentence: ')
-    dataToSend = encryptDeccryptData('12342515',msg,1)
-    dataBytes = bytes(dataToSend,'utf-8')
-  
-    clientSocket.send(dataBytes)
+ 
+    sys.exit()
     
-    if(msg == 'exit'):
-        break
-      
-    response = clientSocket.recv(1024)
-    
-    msgreceived = encryptDeccryptData('12342515',msg,-1)
-    print('From Server :' ,msgReceived)
+else :
+    clientSocket.send('exit'.encode())
+    print('Client registration failed !')
 
--- server.py
+++ server.py
@@ -1,31 +1,138 @@
 from socket import *
 import threading
+import json
+import sys
+import random
+
 serverPort = 8001
-serverSocket = socket()
+serverSocket = socket(AF_INET,SOCK_STREAM)
 serverSocket.bind(('',serverPort))
 serverSocket.listen(5)
 
+dbms_Authenticate = []
+
 print('The server is running at port : ' + str(serverPort))
 
-def multi_thread(client,addr):
-    while 1:
-        print('Current Thread : '+ str(threading.current_thread()))
-        data = client.recv(1024)
-        msg = data.decode('utf-8')
-        print(msg)
-        if msg == 'exit':
-            print('Exiting..')
+def encryptData(senderId,receiverId,msg) :
+    senderPat = ''
+    receiverPat = ''
+
+    for i in range(0,len(senderId),2) :
+        x = int(senderId[i])
+        for j in range(x) :
+            senderPat += senderId[i+1]
+ 
+    for i in range(0,len(receiverId),2) :
+        x = int(receiverId[i])
+        for j in range(x) :
+            receiverPat += receiverId[i+1]
+    
+    text = ''
+    s = 0
+    r = 0
+
+    for i in range(len(msg)) :
+        print(s,r)
+        text += chr(ord(msg[i]) + int(senderPat[s]) - int(receiverPat[r]))
+        s = (s + 1) % len(senderPat)
+        r = (r + 1) % len(receiverPat)
+
+    return text
+
+def getNewId(length) :
+    a = '123456'
+    id = ''
+    for i in range(length) :
+        id += random.choice(a)
+    return id
+
+def getUser(mobNo) :
+    for user in dbms_Authenticate :
+        if user['mobNo'] == mobNo :
+            return user
+    
+def clientManager(client,addr,user):
+    print('Connected to Client : ' + str(user['mobNo']))
+    while True:
+        try :
+            data = client.recv(2048)
+            msg = data.decode()
+            if msg.lower() == 'exit':
+                dbms_Authenticate.remove(user)
+                res = {'error' : True ,'type' : 'exiting...'}
+                data = json.dumps(res)
+                client.send(data.encode())
+                client.close()
+                break  
+            data = json.loads(msg)
+    
+            print(str(data['to']) , ' : ',data['msg'])
+
+            receiver = getUser(data['to'])
+            if receiver == None :
+                res = { 
+                    'error' : True,
+                    'type' : 'Receiver Offline'
+                }
+                res = json.dumps(res)
+                client.send(res.encode())
+            else :
+                text = encryptData(user['id'],receiver['id'],data['msg'])
+                res = {
+                    'from' : receiver['mobNo'],
+                    'msg' : text,
+                    'error' :False
+                }
+                res = json.dumps(res)
+                receiver['ip'].send(res.encode())
+        except :
+            dbms_Authenticate.remove(user)
+            print(str(user['mobNo'] ), ' : ' , 'error')
             break
-        capatilizedSentence = data.upper()
-        client.send(capatilizedSentence)
+
+
+    print('Client : '+ str(user['mobNo']) + ', session ended') 
+
+def registerClient(clientSocket,addr) :
+    global dbms_Authenticate
+    user = {}
+    try : 
+        client = clientSocket.recv(256)
         
-        
-while 1 :
-        connectionSocket,addr = serverSocket.accept()
-        print('Connected to: '+ addr[0] + ':' + str(addr[1]))
-        thread = threading.Thread(target=multi_thread,args=(connectionSocket,addr));
+        user['ip'] = clientSocket
+        user['mobNo'] = int(client.decode())
+        user['id'] = getNewId(10)
+       
+        dbms_Authenticate.append(user)
+
+        thread = threading.Thread(target=clientManager,args=(clientSocket,addr,user))
         thread.start()
+
+        res = { 'error' : False , 'type' :'' ,'id' : user['id'] }
+        print(res)
+        data = json.dumps(res)
+        clientSocket.send(data.encode())
+    except :
+        res = { 'error' : True , 'Type' : 'Invalid Number' }
+        data = json.dumps(res)
+        clientSocket.send(data.encode())
+
+def exitTemp() :
+    i = input()
+    if i == 'q' :
+        serverSocket.close()
+        sys.exit()
+
+while 1 :
+    e = threading.Thread(target=exitTemp, args=())
+    e.start()
+    try :
+        clientSocket,addr = serverSocket.accept()
+        c = threading.Thread(target=registerClient ,args=(clientSocket,addr))
+        c.start()
+    except :
+        print('Error at main server :')
         
-  
+
     
 
